ff3eb1b1fafcb50245a5d52a364c85d12c7325fc
ryuya <user@usernoMacBook-Pro.local>
2022-09-01 19:08:26 +0900

    タイトルを表示する

--- lib/main.dart
+++ lib/main.dart
@@ -20,9 +20,12 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('ホーム'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ToDo'),
+      ),
+      body: const Center(
+        child: Text('メインコンテンツ'),
       )
     );
   }
